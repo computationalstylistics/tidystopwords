bd5e26641419297c9a4ed64fa50fe7eabb34648b
cinkova <cinkova@users.noreply.github.com>
2018-06-23 23:21:06 +0200

    Delete list_supported_languages.R

--- list_supported_languages.R
+++
@@ -1,5 +0,0 @@
-list_supported_languages <- function() {
-  multilingual_stoplist <- read.csv("DATA/multilingual_stoplist.csv")
-  supported_languages <- unique(multilingual_stoplist$language) %>% levels()
-  return(supported_languages)
-}
