c41271d100e97bed5bba6d050a3ae1b491c2a8df
Silvie Cinkova <cinkova@ufal.mff.cuni.cz>
2018-07-02 15:32:35 +0200

    set rda.zip back to rda in this function

--- R/list_supported_language_names.R
+++ R/list_supported_language_names.R
@@ -1,5 +1,5 @@
 list_supported_language_names <- function() {
-    load("sysdata.rda.gzip")
+    load("sysdata.rda")
     supported_languages <- unique(multilingual_stoplist$language_name)
     return(supported_languages)
 }
