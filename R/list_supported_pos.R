8877fd7a9113e0d0d41988ab8fecf24f03352b5e
Silvie Cinkova <cinkova@ufal.mff.cuni.cz>
2018-07-02 12:50:59 +0200

    replacing multilingual_stoplist with sysdata in the script

--- R/list_supported_pos.R
+++ R/list_supported_pos.R
@@ -1,5 +1,5 @@
 list_supported_pos <- function() {
-  multilingual_stoplist <- data(sysdata)
-  supported_languages <- unique(multilingual_stoplist$POS)
+  data(sysdata)
+  supported_languages <- unique(sysdata$POS)
   return(supported_languages)
 }
